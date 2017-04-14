2e75677e3660768c49ab617e139bda53a78e36e8
SuzukiShin-ya <sn-suzuki@techno7.co.jp>
2017-04-14 15:56:38 +0900

    Finish log in/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,9 @@ class UsersController < ApplicationController
   def create
    @user = User.new(user_params)
        if @user.save
+       
+       log_in @user
+       
          flash[:success] = "Welcome to the Sample App!"
          redirect_to @user
     else
@@ -29,5 +32,5 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 
-  
+                     
 end
