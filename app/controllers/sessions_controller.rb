d73fb12df6f0d7d2146c887f7f74993f98e5badc
SuzukiShin-ya <sn-suzuki@techno7.co.jp>
2017-04-18 17:12:09 +0900

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,9 +8,13 @@ class SessionsController < ApplicationController
          user = User.find_by(email: params[:session][:email].downcase)
        if user && user.authenticate(params[:session][:password])
           #ユーザーログイン後にユーザー情報のページにリダイレクトする
+
           log_in user
+
           params[:session][:remember_me] == '1' ? remember(user) : forget(user)   
-          redirect_to user            
+
+          redirect_back_or user           
+
        else
            flash.now[:danger] = 'Invalid email/password combination'   
             #エラーメッセージを作成する
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -15,6 +15,12 @@ module SessionsHelper
   end
 
 
+  # 与えられたユーザーがログイン済みユーザーであればtrueを返す
+  def current_user?(user)
+    user == current_user
+  end
+
+
   # 記憶トークンcookieに対応するユーザーを返す
   def current_user
     if (user_id = session[:user_id])
@@ -55,5 +61,18 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+
+  # 記憶したURL (もしくはデフォルト値) にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
+
+
 
 end
